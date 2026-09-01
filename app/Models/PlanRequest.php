76f1a86b8de6900334c66b5ca1ace187e7c5aa6c
budii <budiiwira@gmail.com>
2026-09-01 17:24:34 +0700

    fix(qa-2): add @property annotations to PlanRequest model
    - Add PHPDoc for tenant, plan, requestedBy, resolvedBy relations
    - Fixes property.notFound errors in BillingRequestController

--- app/Models/PlanRequest.php
+++ app/Models/PlanRequest.php
@@ -6,6 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $tenant_id
+ * @property int $plan_id
+ * @property string|null $note
+ * @property string $status
+ * @property int $requested_by
+ * @property int|null $resolved_by
+ * @property \Illuminate\Support\Carbon|null $resolved_at
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property-read Tenant $tenant
+ * @property-read Plan $plan
+ * @property-read User $requestedBy
+ * @property-read User|null $resolvedBy
+ */
 class PlanRequest extends Model
 {
     use HasFactory;
