e0c1bc61bcd2e69736ecd4b3c07286af61ce4a35
budii <budiiwira@gmail.com>
2026-09-08 15:34:18 +0700

    style: pint fix + docs changelog V3.2.0

--- routes/web.php
+++ routes/web.php
@@ -346,5 +346,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
 require __DIR__.'/auth.php';
